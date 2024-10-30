af4ca051b6c91b84ca51cd05f1068b159d9c214d
Duda <129407882+Duda2606@users.noreply.github.com>
2024-10-29 21:33:56 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/Andressamoraesss/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/Duda2606/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
